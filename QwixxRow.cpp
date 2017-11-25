881db79c70d4873c24ba699d79ff957f50cababa
patrick <patrick.as@live.com>
2017-11-25 17:41:35 -0500

    remove unused fcts

--- QwixxRow.cpp
+++ QwixxRow.cpp
@@ -8,32 +8,15 @@ QwixxRow<std::vector<int>,RED>::QwixxRow() {
 	rScore = std::vector<int>(11, 0);
 }
 
-QwixxRow<std::vector<int>, BLUE>::QwixxRow() {
-	rScore = std::vector<int>(11, 0);
-}
+
 QwixxRow<std::vector<int>, YELLOW>::QwixxRow() {
 	rScore = std::vector<int>(11, 0);
-}
-QwixxRow<std::vector<int>, WHITE>::QwixxRow() {
-	rScore = std::vector<int>(11, 0);
-}
-QwixxRow<std::vector<int>, GREEN>::QwixxRow() {
-	rScore = std::vector<int>(11, 0);
-}
-
-QwixxRow<std::list<int>, RED>::QwixxRow() {
-	rScore = std::list<int>(11, 0);
 }
 
 QwixxRow<std::list<int>, BLUE>::QwixxRow() {
 	rScore = std::list<int>(11, 0);
 }
-QwixxRow<std::list<int>, YELLOW>::QwixxRow() {
-	rScore = std::list<int>(11, 0);
-}
-QwixxRow<std::list<int>, WHITE>::QwixxRow() {
-	rScore = std::list<int>(11, 0);
-}
+
 QwixxRow<std::list<int>, GREEN>::QwixxRow() {
 	rScore = std::list<int>(11, 0);
 }
