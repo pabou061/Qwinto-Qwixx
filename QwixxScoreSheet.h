7157f6b49ef3653898fb7367ccf3a4d16c49f17f
patrick <patrick.as@live.com>
2017-11-25 17:42:08 -0500

    add methods

--- QwixxScoreSheet.h
+++ QwixxScoreSheet.h
@@ -11,6 +11,18 @@ class QwixxScoreSheet : ScoreSheet {
 	QwixxRow <std::list<int>, BLUE> b;
 	QwixxRow <std::vector<int>, YELLOW> y;
 	QwixxRow <std::list<int>, GREEN> g;
+	bool isLocked[4];
 
 	QwixxScoreSheet(std::string name);
+
+	int virtual calcTotal() ;
+	void setTotal();
+	bool const operator!();
+	//friend std::ostream& operator<<(std::ostream& out, ScoreSheet s);
+
+	~QwixxScoreSheet();
+protected:
+	bool validate(RollOfDice rd, Colour selectedC, int positionL = -1);
+
 };
+
