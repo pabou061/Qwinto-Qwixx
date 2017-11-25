6b8378510d6b0a74a7d2bed60555f76977a46c7e
patrick <patrick.as@live.com>
2017-11-25 14:20:33 -0500

    Add functions

--- QwintoScoreSheet.h
+++ QwintoScoreSheet.h
@@ -1,6 +1,25 @@
 #pragma once
 
 #include "ScoreSheet.h"
+#include "QwintoRow.h"
 class QwintoScoreSheet : ScoreSheet {
+
+public:
+	QwintoRow <RED> r;
+	QwintoRow <BLUE> b;
+	QwintoRow <YELLOW> y;
+
+
+	QwintoScoreSheet();
+
+	bool score(RollOfDice, Colour selectedC, int positionL = -1);
+	int calcTotal();
+	void setTotal();
+	bool const operator!();
+	friend std::ostream& operator<<(std::ostream& out, ScoreSheet s);
+
+	~QwintoScoreSheet();
+protected:
+	void validate();
 
 };
