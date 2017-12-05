9e3d969117aeaf8a4876ea071c780487ab475c7d
Gabriel <jchau069@uottawa.ca>
2017-12-05 15:04:33 -0500

    made the isPlaying be initialised to false

--- QwixxPlayer.h
+++ QwixxPlayer.h
@@ -6,7 +6,7 @@ class QwixxPlayer : public Player {
 public:
 	QwixxScoreSheet q;
 	
-	QwixxPlayer(std::string _name, QwixxScoreSheet _scoreSheet) : Player(_name), q(_scoreSheet) {};
+	QwixxPlayer(std::string _name, QwixxScoreSheet _scoreSheet) : Player(_name), q(_scoreSheet) { isPlaying = false; };
 	void inputBeforeRoll(RollOfDice &_rollOfDice);
 	void inputAfterRoll(RollOfDice &_rollOfDice);
 
